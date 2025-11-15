a7849e447a380b6c9abd924e8bfd3bbe554fc4b4
endahh <endahkomarialestari@gmail.com>
2025-11-15 08:33:00 +0800

    Simpan perubahan sementara sebelum pindah branch

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
-<?php
-
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AboutController;
-use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/', [HomeController::class, 'index']);       // Home
+Route::get('/about', [AboutController::class, 'index']); // About
